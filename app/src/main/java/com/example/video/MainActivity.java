64d3ea49ec25d90ce9aabfbc6cc20f88d9ab4feb
anhnguyet2909 <72873569+anhnguyet2909@users.noreply.github.com>
2020-12-22 19:04:48 +0700

    add part 9

--- app/src/main/java/com/example/video/MainActivity.java
+++ app/src/main/java/com/example/video/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.video;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -20,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        if(!checkInternetConnection()){
+            binding.btnReturn.setVisibility(View.VISIBLE);
+            binding.tvCheck.setVisibility(View.VISIBLE);
+            binding.svMain.setVisibility(View.INVISIBLE);
+        }
+        else{
+            setClick();
+        }
+        binding.btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkInternetConnection()){
+                    Toast.makeText(getBaseContext(), getText(R.string.network_confirm), Toast.LENGTH_LONG).show();
+                    binding.svMain.setVisibility(View.VISIBLE);
+                    setClick();
+                    binding.btnReturn.setVisibility(View.INVISIBLE);
+                    binding.tvCheck.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+    }
+
+    public void setClick(){
         getFragment(HotVideosFragment.newInstance());
         binding.llHotVideos.setOnClickListener(v-> {
             getFragment(HotVideosFragment.newInstance());
@@ -63,7 +92,34 @@ public class MainActivity extends AppCompatActivity {
             binding.v3.setVisibility(View.INVISIBLE);
         });
     }
+
     public void getFragment(Fragment fragment){
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+    }
+
+    private boolean checkInternetConnection() {
+        // Get Connectivity Manager
+        ConnectivityManager connManager =
+                (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Details about the currently active default data network
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+
+        if (networkInfo == null) {
+            Toast.makeText(this, getText(R.string.network_info), Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (!networkInfo.isConnected()) {
+            Toast.makeText(this, getText(R.string.network_connected), Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (!networkInfo.isAvailable()) {
+            Toast.makeText(this, getText(R.string.network_available), Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
     }
 }
